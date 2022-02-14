3be06855e1d2006e023f996e23dfabacbcc854c8
helenozzuil <helen.liuzzo@gmail.com>
2022-02-14 12:33:19 +0100

    javascript1

---
+++ s4/g1/js/myscript.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    document.getElementById('colore').style.color = 'green';
+}
+
+function changeFont() {
+    document.getElementById('para').style.textTransform = 'uppercase';
+}
+
+function somma() {
+    2 + 2;
+}
+
+console.log(somma());
